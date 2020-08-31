55dc570cc30e79c1d410b0f55786ba48b9394169
ahmad magdy <ahmad.magdy@nebrastech.com>
2020-08-31 20:25:16 +0200

    add gender to student model

--- School/Models/Student.cs
+++ School/Models/Student.cs
@@ -8,6 +8,7 @@ namespace School.Models
     public class Student
     {
        public string Name { get; set; }
+       public string Gender { get; set; }
        public string Phone { get; set; }
        public string Email { get; set; }
        public string Address { get; set; }
