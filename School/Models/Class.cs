7dcdc84dd66c1fc786aae213fea833a4d74287cd
ahmad magdy <ahmad.magdy@nebrastech.com>
2020-08-31 20:16:49 +0200

    Add project files.

---
+++ School/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School.Models
+{
+    public class Class
+    {
+        public int Floor { get; set; }
+        public string Name { get; set; }
+    }
+}
