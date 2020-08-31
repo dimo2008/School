d0e28d7c063cf2c4053a850796cb0fd643846af0
ahmad magdy <ahmad.magdy@nebrastech.com>
2020-08-31 20:25:45 +0200

    add mobile to student model

--- School/Models/Student.cs
+++ School/Models/Student.cs
@@ -10,6 +10,7 @@ namespace School.Models
        public string Name { get; set; }
        public string Gender { get; set; }
        public string Phone { get; set; }
+       public string Mobile { get; set; }
        public string Email { get; set; }
        public string Address { get; set; }
        public float GPA { get; set; }
